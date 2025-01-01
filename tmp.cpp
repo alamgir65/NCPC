3169d250166b2af6a1b84ec552d5b82a0a755263
unknown <alamgirhossaindrubo@gmail.com>
2025-01-01 11:22:21 +0600

    first commit

---
+++ tmp.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define out(x) cout<<x<<'\n';
+using namespace std;
+void solve(){
+    
+}
+int main()
+{
+    int t=1; 
+    cin>>t;
+    while(t--) solve();
+    return 0;
+} 
