7fcfa0a755e2c72838a353000575904ff3c03ad3
unknown <alamgirhossaindrubo@gmail.com>
2025-04-18 23:31:17 +0600

    hudai 2

---
+++ B_Team_Training.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define all(x) x.begin(), x.end()
+#define nl cout<<endl;
+#define endl '\n'
+#define love int main()
+#define vtr vector
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define Alamgir ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+void solve(){
+    ll n,x; cin>>n>>x;
+    vll a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    sort(a.rbegin(),a.rend());
+    ll ans=0;
+    int i=n-1;
+    int cnt=0;
+    for(auto i:a){
+        cnt++;
+        if(i*cnt >= x){
+            ans++;
+            cnt=0;
+        }
+    }
+    out(ans)
+}
+love{
+    Alamgir
+    int t=1; 
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        solve();
+    }
+    return 0;    
+}
