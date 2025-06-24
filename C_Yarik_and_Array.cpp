4d9fc582905cb38d227bfdfdd89ed4f059a3409e
unknown <alamgirhossaindrubo@gmail.com>
2025-06-25 02:12:55 +0600

    Kadane's algorithms problem added

---
+++ C_Yarik_and_Array.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define all(x) x.begin(), x.end()
+#define nl cout<<endl;
+#define endl '\n'
+#define love int main()
+#define vtr vector
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define Alamgir ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+void solve(){
+    int n; cin>>n;
+    vii a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    ll mx=a[0],sum=a[0];
+    for(int i=1;i<n;i++){
+        if(abs(a[i-1])%2 == abs(a[i])%2) sum = 0;
+        sum = max(sum + a[i],1LL * a[i]);
+        mx = max(mx, sum);
+    }
+    out(mx)
+}
+love{
+    Alamgir
+    int t=1; 
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        solve();
+    }
+    return 0;    
+}
