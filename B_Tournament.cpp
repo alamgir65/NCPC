1f1dfe7cb99f6891bda3a6ba1bf9f3f651b1164c
unknown <alamgirhossaindrubo@gmail.com>
2025-07-01 23:02:46 +0600

    div 3

---
+++ B_Tournament.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define all(x) x.begin(), x.end()
+#define nl cout<<endl;
+#define endl '\n'
+#define love int main()
+#define vtr vector
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define Alamgir ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+void solve(){
+    int n,j,k; cin>>n>>j>>k;
+    vii a(n);
+    // map<int,int> mp;
+    for(int i=0;i<n;i++) cin>>a[i];
+    int track=a[j-1];
+    sort(all(a));
+    if(track!=a[n-1] && k==1) no
+    else yes
+}
+love{
+    Alamgir
+    int t=1; 
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        solve();
+    }
+    return 0;    
+}
---
+++ C_Prefix_Min_and_Suffix_Max.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int &i : a) cin >> i;
+  string ans = string(n, '0');
+  int mn = 1e9 + 5, mx = 0;
+  for (int i = 0; i < n; i++) {
+    mn = min(mn, a[i]);
+    if (mn == a[i]) ans[i] = '1';
+  }
+  for (int i = n - 1; i >= 0; i--) {
+    mx = max(mx, a[i]);
+    if (mx == a[i]) ans[i] = '1';
+  }
+  cout << ans << "\n";
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    testCase();
+  }
+}
