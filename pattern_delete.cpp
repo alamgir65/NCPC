5fcde14134016eb04a3e42b56ebad9eb30ee8115
unknown <alamgirhossaindrubo@gmail.com>
2025-08-31 01:24:48 +0600

    added kichu akta

---
+++ pattern_delete.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string text, pattern;
+    cout << "Enter text: ";
+    getline(cin, text);
+
+    cout << "Enter pattern to delete: ";
+    getline(cin, pattern);
+
+    size_t pos;
+    while ((pos = text.find(pattern)) != string::npos) {
+        text.erase(pos, pattern.size());
+    }
+
+    cout << "After deletion: " << text << "\n";
+    return 0;
+}
